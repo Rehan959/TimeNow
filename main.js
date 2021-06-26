bc1a80e619abc042e1ec66b18045597e9fc2bc1f
Rehan959 <rehanshami007@gmail.com>
2021-06-26 09:10:52 +0530

    Time Now

--- main.js
+++ main.js
@@ -5,9 +5,22 @@ function Time_now(){
    var hr = dt.getHours();
    var min = dt.getMinutes();
  var sec = dt.getSeconds();
-  if(hr >12){    
-     hr = hr-12;
- }
+
+ if(hr >12){
+     AM="PM";
+    }
+ if(hr ==12){
+        AM="PM";
+    }
+    
+    if(hr < 12){
+        AM="AM"
+    }
+
+
+    if(hr >12){
+        hr = hr-12;
+    }
 
  if(hr<10)
  {
@@ -19,10 +32,29 @@ function Time_now(){
       min = "0"+min
   } 
 
+  if(hr>6 && hr<18){
+      Day_Mode();
+    }else{
+     Night_Mode();
+    }
 
  if(sec <10)
 {
     sec ="0"+sec
 }
- Time.innerHTML=`${hr}:${min}:${sec}`;
+ Time.innerHTML=`${hr}:${min}:${sec}  ${AM}`;
+
+ setTimeout(Time_now,1000);
+}
+
+function Night_Mode(){
+    var Body = document.querySelector("#Body");
+    Body.style.background="black"
+    Body.style.color = "White"
+}
+
+function Day_Mode (){
+    var Body = document.querySelector("#Body");
+    Body.style.background="white";
+    Body.style.color = "black";
 }
